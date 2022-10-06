6e18a514e334f379fe1a7b41d53f586662c6e842
Thiên Long <nguyenkhoathienlong313@gmail.com>
2022-10-06 10:02:50 +0700

    update

--- BikeService/BikeService/Controllers/ManufacturerController.cs
+++ BikeService/BikeService/Controllers/ManufacturerController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using BikeService.Data;
+using BikeService.DTOs.Request;
+using BikeService.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BikeService.Controllers
@@ -7,5 +11,25 @@ namespace BikeService.Controllers
     [ApiController]
     public class ManufactuerController : ControllerBase
     {
+        private readonly MyDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ManufactuerController(MyDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [ActionName("AddManufacturer")]
+        [HttpPost]
+        public async Task<ActionResult<Manufacturer>> Add(ManufacturerRequest manufactureRequest)
+        {
+            Manufacturer manufacturer = new Manufacturer();
+            manufacturer.Name = manufactureRequest.Name;
+            _context.Manufacturers.Add(manufacturer);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("AddManufacturer", new { id = manufacturer.Id , manufactureRequest});
+        }
+
     }
 }
